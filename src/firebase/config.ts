b59e4a4b069cddbdfc01b56bc49d623aaebfc3e1
Mücteba KÖKSAL <muctebakoksal908@gmail.com>
2025-11-17 08:25:38 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-4483894050-20756",
+  "appId": "1:966843937382:web:eefb0da8e28a34662fb3a7",
+  "apiKey": "AIzaSyDbPUR8ZqGS8JS27Giv8B4GKuINbEOm0zo",
+  "authDomain": "studio-4483894050-20756.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "966843937382"
+};
